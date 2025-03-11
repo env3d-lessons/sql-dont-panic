27a6ac1137a07ad436fe865d3f02c70116e41725
env3d <jmadar@gmail.com>
2025-03-10 18:22:31 -0700

    reset solution

--- 1.sql
+++
@@ -1,1 +0,0 @@
-update users set password = '1c658300a285a58aa25d7bd133ca9896' where username = 'admin';
--- 2.sql
+++
@@ -1,3 +0,0 @@
--- modify log entry
-update user_logs set new_password = (select password from users where username = 'emily33')
-where type = 'update' and old_username = 'admin' and new_username = 'admin';
